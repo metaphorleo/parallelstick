e872afb1e023bc95951d37ecb04ddf3e284a98ab
metaphorleo <leo.trin.dade1999@gmail.com>
2022-07-12 17:18:45 -0300

    done

---
+++ Atividades Dev Python/M1S4/teste.py
@@ -0,0 +1,8 @@
+tt = "1a12b123c"
+
+xx = list(tt)
+
+for i in xx:
+    if i == "1":
+        print(i)
+
---
+++ conversor.py
@@ -0,0 +1,8 @@
+import os
+
+dir = ("C:\\Users\\Leonardo\\Documents\\GitHub\\parallelstick\\Nova Pasta")
+for i in os.listdir(dir):
+    files = os.path.join(dir, i)
+    split = os.path.splitext(files)
+    if split[1] == ".py":
+       os.rename(files, split[0] + ".txt")
